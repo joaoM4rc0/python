e52fac9f2b7711d5c73c2ca2300193e7b80394bf
João marco <140863268+joaom4rco@users.noreply.github.com>
2024-03-30 19:28:38 -0300

    Add files via upload

---
+++ poo_novas_aulas/aula24.py
@@ -0,0 +1,18 @@
+def meu_repr(self):
+    class_name = self.__class__.__name__
+    class_dict = self.__dict__
+    class_repr = f'{class_name} ({class_dict})'
+    return class_repr
+def adiciona_repr(cls):
+    cls.__repr__ = meu_repr
+    return cls
+@adiciona_repr
+class Time:
+    def __init__(self, nome):
+        self.nome = nome
+@adiciona_repr
+class planeta:
+    def __init__(self, nome):
+        self.nome = nome
+time1 = Time('Brasil')
+print(time1)
