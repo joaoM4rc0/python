19c3857bb8597e3a109b21db4151cb657717e576
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-03 16:27:08 -0300

    Adicionando aulas e outras mudanças

--- exer/exer11.py
+++ exer/exer11.py
@@ -1,27 +1,22 @@
-import os
+import os   
 lista = []
 while True:
-
     print('selecione uma opção')
-    opcoes = input('[i]nserir [a]pagar [l]istar ')
+    opcoes = input('[i]nserir [a]pagar [l]istar ').lower()
     opcoes_permitidas = 'ial'
-
     if opcoes not in opcoes_permitidas:
         print('não é possivel fazer nada')
-        
+    
     if opcoes == 'i':
-        lista_add_item = input('adcione o que voce quiser na lista ')
+        lista_add_item = input('adicione oq quiser na lista: ')
         lista.append(lista_add_item)
-        os.system('cls')
         for lista_enumerada in enumerate(lista):
-            os.system('cls')
             indice, nome = lista_enumerada
             print(indice, nome)
-
     if opcoes == 'a':
-        lista_del_item = input('escolha o indice para apagar ')
+        lista_del_item = input('digite um indice para apagar: ')
         os.system('cls')
-        try: 
+        try:
             int_lista = int(lista_del_item)
             if int_lista == indice:
                 lista.pop(int_lista)
@@ -31,10 +26,9 @@ while True:
             print('você tem que adicionar um elemento a sua lista ')
         if int_lista > len(lista):
             print('esse índice não existe')
-
     if opcoes == 'l':
-        os.system('cls')
         if len(lista) == 0:
-            print('nada para listar')
+            os.system('cls')
+            print('não tem nada para listar')
         for i, valor in enumerate(lista):
             print(i, valor)
